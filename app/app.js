7c4683b6461dff7b8a67444774fb1718c4bd2879
Talal Arshad <talal7860@gmail.com>
2017-12-24 15:48:34 +0500

    adding bootstrap

--- app/app.js
+++ app/app.js
@@ -16,6 +16,7 @@ import { ConnectedRouter } from 'react-router-redux';
 import FontFaceObserver from 'fontfaceobserver';
 import createHistory from 'history/createBrowserHistory';
 import 'sanitize.css/sanitize.css';
+import 'bootstrap/dist/css/bootstrap.css'; // eslint-disable-line
 
 // Import root app
 import App from 'containers/App';
